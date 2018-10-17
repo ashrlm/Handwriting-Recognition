1e435c4f29ea2a6b585e4dc426f31d69dd4a94bb
Asher Matheson <ashrlmthsn@gmail.com>
2018-10-17 13:22:41 +1300

    Added Input/Hidden generation

--- main.py
+++ main.py
@@ -1,20 +1,52 @@
-#!usr/bin/env/python3
-
 import math
 
 import sympy #Calculation of partial derivatives
 from mnist import MNIST #Reading of datasets
 
 class Network():
-    def __init__(self,layers,  neurons, connections):
-        self.layers = layers
-        self.neurons = neurons
-        self.connections = connections
+    def __init__(self):
+        self.layers = [Layer()]
+        self.neurons = []
+        self.connections = []
+        self.input_neurons = []
+
+        for i in range(784): #Input layer generation
+            input_neuron = Neuron([])
+            self.neurons.append(input_neuron)
+            self.layers[0].neurons.append(input_neuron)
+            self.input_neurons.append(input_neuron)
+
+        for i in range(2): #Hidden layer generation
+            self.layers.append(Layer([]))
+            for i in range(10):
+                new_neuron = Neuron()
+                self.neurons.append(new_neuron)
+                for neuron in self.layers[-1].neurons:
+                    new_conn = Connection(
+                        (neuron, new_neuron)
+                    )
+
+                    new_neuron.inputs.append(new_conn)
+                    self.connections.append(new_conn)
+
+        for i in range(10): #Output layer generation
+            pass
+
+
 
     def activate(self):
         for layer in self.layers:
             for neuron in layer.neurons:
                 neuron.activate()
+
+    def cost(self):
+        pass
+
+    def part_deriv(self):
+        pass
+
+    def gradient_descent(self):
+        pass
 
 class Layer():
     def __init__(self, neurons):
@@ -27,7 +59,7 @@ class Connection():
 
 class Neuron():
     def __init__(self, inputs, output=None):
-        if self.output:
+        if output:
             self.output = output
             self.input_neuron = True
         self.inputs = inputs #List of connection objects
@@ -41,3 +73,16 @@ class Neuron():
 
 def sigmoid(x):
     return 1/(1+(math.e ** -x))
+
+def load_dataset(ds_path, training=True):
+    dataset = MNIST(ds_path)
+    if training:
+        return dataset.load_training()
+    else:
+        return dataset.load_testing()
+
+def main():
+    Network()
+
+if __name__ == "__main__":
+    main()
