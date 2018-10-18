3c57d8e181cf34fb7f246397de8dd9ed496722e8
Asher Matheson <ashrlmthsn@gmail.com>
2018-10-19 11:02:40 +1300

    Setup minibatches

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import math
 import random
 
-import sympy #Calculation of partial derivatives
 from mnist import MNIST #Reading of datasets
 
 class Network():
-    def __init__(self):
+    def __init__(self, ds, mbs=100, seed=1):
+        self.dataset = load_dataset(ds, False) #Pull dataset, seperate for efficiency
+        self.min_batch_size = mbs
+        self.img_sets = {}
+
+        for ds in self.dataset: #Randomly shuffle datasets
+            random.seed(seed) #Seed to ensure images and values stay same
+            random.shuffle(ds)
+
+        for img, val in zip(self.dataset[0], self.dataset[1]):
+            self.img_sets[tuple(img)] = val
+
+        print(self.img_sets)
+
+
         self.layers = [Layer([])]
         self.neurons = []
         self.connections = []
@@ -40,39 +53,41 @@ class Network():
                 new_neuron.inputs.append(new_conn)
                 self.connections.append(new_conn)
 
-    def activate(self, img):
+    def activate(self):
 
-        for input_neuron, px_val in zip(self.layers[0].neurons, img):
-            input_neuron.output = px_val
+        # for img in self.dataset[0]:
+#
+            # for input_neuron, px_val in zip(self.layers[0].neurons, img):
+                # input_neuron.output = px_val
+                # print(type(px_val))
+#
+        # for layer in self.layers[1:]:
+            # for neuron in layer.neurons:
 
-        for layer in self.layers[1:]:
-            for neuron in layer.neurons:
-                neuron.activate()
-
-    def cost(self):
-        dataset = load_dataset('dataset')
-        train_img = list(dataset[0][0])
-        train_labels = dataset[1]
-
-        costs = []
-
-        for img, label in zip(train_img, train_labels):
-
-            self.activate(img)
-
-            expected_out = [0] * 10
-            expected_out[label] = 1
-
-            for neuron, expected in zip(self.layers[-1].neurons, expected_out):
-                costs.append((neuron.output - expected) ** 2)
-
-        self.cost = sum(cost) / len(cost)
-
-
-    def part_deriv(self):
         pass
 
-    def gradient_descent(self):
+
+    def cost(self):
+        # dataset = load_dataset('dataset')
+        # train_img = list(dataset[0][0])
+        # train_labels = dataset[1]
+#
+        # costs = []
+#
+        # for img, label in zip(train_img, train_labels):
+#
+            # self.activate(img)
+#
+            # expected_out = [0] * 10
+            # expected_out[label] = 1
+#
+            # for neuron, expected in zip(self.layers[-1].neurons, expected_out):
+                # costs.append((neuron.output - expected) ** 2)
+
+            pass
+
+
+    def backprop(self):
         pass
 
 class Layer():
@@ -95,7 +110,7 @@ class Neuron():
 
     def activate(self):
         if not self.input_neuron:
-            weighted_input = 0
+            weighted_input = self.bias
             for input in self.inputs:
                 weighted_input += input.neurons[0].output * input.weight
             self.output = sigmoid(weighted_input)
@@ -115,13 +130,12 @@ def load_dataset(ds_path, training=True):
     # NOTE: Returns (Dataset images, Dataset labels)
     dataset = MNIST(ds_path)
     if training:
-        return (dataset.load_training(), dataset.train_labels)
+        return (dataset.load_training()[0], dataset.train_labels)
     else:
-        return (dataset.load_testing(), dataset.test_labels)
+        return (dataset.load_testing()[0], dataset.test_labels)
 
 def main():
-    v=Network()
-    v.cost()
+    Network('dataset')
 
 if __name__ == "__main__":
     main()
